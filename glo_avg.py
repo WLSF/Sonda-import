6f2f3e4b5206ab0600ab3cc77b415115e46b6843
Luiz Felipe das Neves Lopes <luizfelipe2310@gmail.com>
2018-08-30 08:16:25 -0300

    Update glo_avg.py

--- glo_avg.py
+++ glo_avg.py
@@ -4,18 +4,18 @@ import csv
 x=[]
 y=[]
 
+vezes = 0 # Numero do dia, valor inicial = 0, a 30.
+line_count = 0 # Variavel do contador de linhas.
+
 # http://sonda.ccst.inpe.br/infos/variaveis.html
 # http://sonda.ccst.inpe.br/basedados/index.html
 
-vezes = 0
-line_count = 0
-
-
 # Conforme é chamado a função myrange o numero de vezes é incrementado.
-
 # 1 vez ou 1 dia = 1439 minutos --- grafico do minuto 0 ao 1439.
-
 # 2 vez ou 2 dia = 2878 minutos --- grafico do minuto 1439 ao 2878.
+
+# Problema 1 : A partir do segundo dia o gráfico é plotado com as informações e cordenadas do primeiro dia/gráfico, como também ignora a area de plotagem.
+# Problema 2 : A partir do segundo gráfico o mesmo imprime as cordenadas iguais ao primeiro, problema talvez relacionado ao array.
 
 def myrange():
     with open('BRB1511ED.csv', 'r') as csvfile:
@@ -24,39 +24,29 @@ def myrange():
         global vezes
         vezes += 1 
         for row in plots:
-            line_min = (vezes-1*1439)
-            line_max = ((vezes*1439)+1)
+            line_min = (vezes-1*1439) # minuto inicial
+            line_max = ((vezes*1439)+1) # minuto final
             
-            if  line_min < line_count & line_count < line_max:   # não funciona como deveria!
-                x.append(int(row[3]))
-                y.append(float(row[4]))
-            line_count += 1
+            if  line_min < line_count & line_count < line_max:   # < Problema 2 possivelmente está aqui!
+                x.append(int(row[3])) # Coluna minutos
+                y.append(float(row[4])) # Coluna Irradiância
 
-	
-def myrange2():
-    with open('BRB1511ED.csv', 'r') as csvfile:
-        plots= csv.reader(csvfile, delimiter=';')
-        global line_count
-        global vezes
-        vezes += 1
-        for row in plots:
-            line_min = (vezes-1*1439)
-            line_max = ((vezes*1439)+1)
-            
-            if line_count > line_min:
-                if line_count < line_max:
-                    x.append(int(row[3]))
-                    y.append(float(row[4]))
-                else:
-                    x.append(int(row[3]))
-                    y.append(0)
-                    
-            else:
-                x.append(int(row[3]))
-                y.append(0)
+
+           #   Outro metodo para tentar especificar o range.
+           #
+           #   if line_count > line_min:
+           #      if line_count < line_max:
+           #             x.append(int(row[3]))
+           #             y.append(float(row[4]))
+           #         else:
+           #             x.append(int(row[3]))
+           #             y.append(0)
+           #      else:
+           #          x.append(int(row[3]))
+           #          y.append(0)  
+           # 
                 
-            line_count += 1
-	
+            line_count += 1  # Incremento ao contador de linhas, mesmo caso não entre no if.
 		
 # Gambiarra para ler uma celula especifica!
 def read_cell(x, y):
@@ -67,15 +57,20 @@ def read_cell(x, y):
             if y_count == y:
                 return row[x]
             y_count += 1
-                
-myrange()
-	
-plt.figure(1)
-plt.subplot(211)
-plt.plot(x,y, 'b-') #b- é azul
 
-plt.title("Radiação Global Horizontal - Rede Sonda - Dia " + str(read_cell(2,((vezes)*1439))))
-plt.ylabel('Irradiância (Wm-2)')
-plt.xlabel('Minuto')
+# Imprime os graficos
+def print():
+    global vezes
+    myrange()
+    plt.figure(vezes-1)  # Numero da figura a ser apresentada.
+    plt.plot(x,y, 'b-') # b- é azul
+    plt.title("Radiação Global Horizontal - Rede Sonda - Dia " + str(read_cell(2,((vezes)*1439))))
+    plt.ylabel('Irradiância (Wm-2)')
+    plt.xlabel('Minuto')
 
-plt.show()
+print() # dia 1
+print() # dia 2
+print() # dia 3
+
+plt.tight_layout() # Arruma o espaçamento entre gráficos, caso use o subplot.
+plt.show() # Imprime os gráficos
