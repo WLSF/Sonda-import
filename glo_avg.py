2f81978eb0a682a355f341f43f60bb1f887d4ca0
Luiz Felipe das Neves Lopes <luizfelipe2310@gmail.com>
2018-08-30 12:55:33 -0300

    Update glo_avg.py

--- glo_avg.py
+++ glo_avg.py
@@ -4,8 +4,9 @@ import csv
 x=[]
 y=[]
 
-vezes = 0 # Numero do dia, valor inicial = 0, a 30.
+vezes = 2 # Numero do dia, valor inicial = 0, a 30.
 line_count = 0 # Variavel do contador de linhas.
+
 
 # http://sonda.ccst.inpe.br/infos/variaveis.html
 # http://sonda.ccst.inpe.br/basedados/index.html
@@ -17,6 +18,7 @@ line_count = 0 # Variavel do contador de linhas.
 # Problema 1 : A partir do segundo dia o gráfico é plotado com as informações e cordenadas do primeiro dia/gráfico, como também ignora a area de plotagem.
 # Problema 2 : A partir do segundo gráfico o mesmo imprime as cordenadas iguais ao primeiro, problema talvez relacionado ao array.
 
+
 def myrange():
     with open('BRB1511ED.csv', 'r') as csvfile:
         plots= csv.reader(csvfile, delimiter=';')
@@ -24,27 +26,11 @@ def myrange():
         global vezes
         vezes += 1 
         for row in plots:
-            line_min = (vezes-1*1439) # minuto inicial
-            line_max = ((vezes*1439)+1) # minuto final
-            
+            line_min = ((vezes-1)*1439) # minuto inicial
+            line_max = ((vezes*1439)+1) # minuto final   
             if  line_min < line_count & line_count < line_max:   # < Problema 2 possivelmente está aqui!
-                x.append(int(row[3])) # Coluna minutos
+                x.append(int(row[3])+((vezes-1)*1439)) # Coluna minutos
                 y.append(float(row[4])) # Coluna Irradiância
-
-
-           #   Outro metodo para tentar especificar o range.
-           #
-           #   if line_count > line_min:
-           #      if line_count < line_max:
-           #             x.append(int(row[3]))
-           #             y.append(float(row[4]))
-           #         else:
-           #             x.append(int(row[3]))
-           #             y.append(0)
-           #      else:
-           #          x.append(int(row[3]))
-           #          y.append(0)  
-           # 
                 
             line_count += 1  # Incremento ao contador de linhas, mesmo caso não entre no if.
 		
