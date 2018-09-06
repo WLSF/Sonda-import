5a907836d847c8374c6ced5c1c710a25f2ba14ad
Luiz Felipe das Neves Lopes <luizfelipe2310@gmail.com>
2018-09-06 02:45:56 -0300

    Update glo_avg.py

--- glo_avg.py
+++ glo_avg.py
@@ -4,27 +4,13 @@ import numpy as np
 # 1 -  Plotagem conforme os dias ao inves dá posição do array, para caso exista alguma falha no banco de dados.
 # 2 -  Detectar se o mês tem 31 dias, ou menos.
 
-<<<<<<< HEAD
 dia= 1 # ~Dia Inicial
-max_dia = 30# ~Dia Final or Max day
+max_dia = 31 # ~Dia Final or Max day
 
 def printa():
     global dia
     lin_min = ((dia-1)*1439)
     lin_max = ((dia)*1439)
-=======
-dia= 0 # ~Dia Inicial
-max_dia = 30 # ~Dia Final or Max day
-
-def printa():
-    global dia
-    dia+= 1
-    lin_min = ((dia-1)*1439)
-    lin_max = (dia*1439)
-<<<<<<< HEAD
->>>>>>> e1b96b72460c25bfa4808fd828e77f37f3c776ec
-=======
->>>>>>> e1b96b72460c25bfa4808fd828e77f37f3c776ec
     x = data[lin_min:lin_max,3]
     y = data[lin_min:lin_max,4]
     plt.figure(dia)
@@ -32,6 +18,7 @@ def printa():
     plt.title("Radiação Global Horizontal - Rede Sonda - Dia " + str(dia))
     plt.ylabel('Irradiância (Wm-2)')
     plt.xlabel('Minuto')
+    dia+= 1
 
 # Lê a planilha
 with open('BRB1511ED.csv', 'r') as csvfile:
