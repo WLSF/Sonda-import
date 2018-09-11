4d49e05d2b71deacca31641958af4a01cacfe908
Luiz Felipe das Neves Lopes <luizfelipe2310@gmail.com>
2018-09-11 14:03:38 -0300

    Update glo_avg.py

--- glo_avg.py
+++ glo_avg.py
@@ -1,34 +1,56 @@
 import matplotlib.pyplot as plt
-import numpy as np
+import csv
 
-# 1 -  Plotagem conforme os dias ao inves dá posição do array, para caso exista alguma falha no banco de dados.
-# 2 -  Detectar se o mês tem 31 dias, ou menos.
+# http://sonda.ccst.inpe.br/infos/variaveis.html
+# http://sonda.ccst.inpe.br/basedados/index.html
+            
+def plot_sonda():
+    with open('BRB1511ED.csv', 'r') as csvfile:
+        plots= csv.reader(csvfile, delimiter=';')
+        x=[]
+        y=[]
 
-dia= 1 # ~Dia Inicial
-max_dia = 30 # ~Dia Final or Max day
+        # Detecta a versao do cabecalho
+        for row in plots:
+            if(row[3].isdigit()): mode(1)
+            else: mode(0)
+            break
+        
+        # Pega o dia inicial
+        for row in plots:
+            dia = row[col_dia]
+            break
+        
+        # Faz a plotagem dos graficos 
+        for row in plots:
+            if(dia != row[col_dia]):
+                plt.figure(dia)
+                plt.plot(x,y, 'b-') #b- é azul
+                plt.title("Radiação Global Horizontal - Rede " + rede + " - Dia " + str(dia))
+                plt.ylabel('Irradiância (Wm-2)')
+                plt.xlabel('Minuto')
 
-def printa():
-    global dia
-    lin_min = ((dia-1)*1439)
-    lin_max = ((dia)*1439)
-    x = data[lin_min:lin_max,3]
-    y = data[lin_min:lin_max,4]
-    
-	plt.figure(dia)
-    plt.plot(x,y, '-b') # b- é azul
-    plt.title("Radiação Global Horizontal - Rede Sonda - Dia " + str(dia))
-    plt.ylabel('Irradiância (Wm-2)')
-    plt.xlabel('Minuto')
-    dia+= 1
+                x.clear()
+                y.clear()   
+                dia = row[col_dia]
+                
+            x.append(int(row[col_min]))
+            y.append(float(row[col_irrad]))
 
-# Lê a planilha
-with open('BRB1511ED.csv', 'r') as csvfile:
-    data = np.genfromtxt(csvfile, delimiter=';')
-    
-# Gráfico dos max_dias dias.
-while(dia <= max_dia): 
-    printa()
+# Cabecalhos
+def mode(x):
+        global col_dia, col_min, col_irrad, rede
+        if x == 0: # Sonda Antigo 
+            rede = 'Sonda'
+            col_dia = 2
+            col_min = 4
+            col_irrad = 5
+            
+        elif(x == 1): # Sonda Novo
+            rede = 'Sonda'
+            col_dia = 2
+            col_min = 3
+            col_irrad = 4
 
-# Arruma o espaçamento entre gráficos, caso use o subplot.
-plt.tight_layout()
+plot_sonda()
 plt.show()
