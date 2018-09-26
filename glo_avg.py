fd83af5f0fe124081204ae718a9dc18731222191
Luiz Felipe das Neves Lopes <luizfelipe2310@gmail.com>
2018-09-26 15:02:52 -0300

    *

--- glo_avg.py
+++ glo_avg.py
@@ -21,10 +21,13 @@ ymensal=[]
 
 def plot_sonda():
     with open(planilha, 'r') as csvfile:
-        plots= csv.reader(csvfile, delimiter=';')
+        plots = csv.reader(csvfile, delimiter=';')
+        global dia, diainicial, media, soma, total, somamensal, totalmensal
         total = 0
         soma = 0
-
+        somamensal = 0
+        totalmensal = 0
+        
         # Detecta a versao do cabecalho
         for row in plots:
             if(row[3].isdigit()): versao(1)
@@ -34,54 +37,96 @@ def plot_sonda():
         # Pega o dia inicial
         for row in plots:
             dia = row[col_dia]
+            diainicial = row[col_dia]
             break
         
-        # Faz a plotagem dos graficos 
+        # Plotagem diaria
         for row in plots:
             if(dia != row[col_dia]):
-
-                plt.figure(dia)
-                plt.plot(x,y, 'b-') #b- é azul
-                plt.title("Rede Sonda - " + planilha[:7] +  " - Dia [" + str(dia) + "]")
-                plt.ylabel('Irradiância (Wm-2)')
-                plt.xlabel('Tempo (Horas)')
-                plt.ylim(0, 1600)
-
-                media = soma/total
-                plt.text(0.35, 1400, 'Média: %5.2f' % media, bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
-                xmensal.append(dia)
-                ymensal.append(media)
-
-
-                x.clear()
-                y.clear()   
-                dia = row[col_dia]
-                soma = 0
-                total = 0
+                diaria();
                 
+            dia = row[col_dia]  
             x.append(horamin(int(row[col_min])))
             y.append(float(row[col_irrad]))
             soma += float(row[col_irrad])
             total += 1
+            
+        # Plotagem do ultimo dia,
+        # pois não há um próximo dia para realizar a comparação.
+        diaria();
 
+        # Plotagem mensal 
+        mensal();
+
+# Plotagem diaria
+def diaria():
+    global dia, diainicial, media, soma, total, somamensal, totalmensal
+    plt.figure(dia)
+    plt.plot(x,y, 'b-') #b- é azul
+    plt.title("Rede Sonda - " + planilha[:7] +  " - Dia [" + str(dia) + "]")
+    plt.ylabel('Irradiância (Wm-2)')
+    plt.xlabel('Tempo (Horas)')
+    plt.ylim(0, 1600)
+
+    # Media
+    media = soma/total
+    plt.text(0.35, 1400, 'Média: %5.2f' % media, bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
+
+
+    diaplot  = (int(dia)-(int(diainicial)-1))
+    xmensal.append(diaplot)
+    ymensal.append(media)
+
+    # Media Mensal
+    somamensal += media
+    totalmensal += 1
+
+    # Limpa as Variaveis
+    x.clear()
+    y.clear()   
+    soma = 0
+    total = 0
+
+
+# Plotagem Mensal
+def mensal():
+    global media, soma, total, somamensal, totalmensal
+    plt.figure(1000)
+    plt.plot(xmensal,ymensal, 'b-') #b- é azul
+    plt.title("Rede Sonda - " + planilha[:7] +  " - Média Mensal")
+    plt.ylabel('Irradiância (Wm-2)')
+    plt.xlabel('Dia')
+    plt.ylim(0, 1600)
+    plt.xlim(1,31)
+    
+    # Media
+    mediamensal = somamensal/totalmensal
+    plt.text(3, 1400, 'Média: %5.2f' % mediamensal, bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
+
+    # Limpa as Variaveis
+    xmensal.clear()
+    ymensal.clear()   
+    somamensal = 0
+    totalmensal = 0    
+    
 # Cabecalhos
 def versao(x):
-        global col_dia, col_min, col_irrad, rede
-        if x == 0: # Sonda Antigo 
-            col_dia = 2
-            col_min = 4
-            col_irrad = 5
+    global col_dia, col_min, col_irrad, rede
+    if x == 0: # Sonda Antigo 
+       col_dia = 2
+       col_min = 4
+       col_irrad = 5
             
-        elif(x == 1): # Sonda Novo
-            col_dia = 2
-            col_min = 3
-            col_irrad = 4
+    elif(x == 1): # Sonda Novo
+        col_dia = 2
+        col_min = 3
+        col_irrad = 4
 
 # Converte minutos em horas
 def horamin(x):
-        hora = int(x/60)
-        minuto = (x%60)/100
-        return (hora + minuto) 
+    hora = int(x/60)
+    minuto = (x%60)/100
+    return (hora + minuto) 
 
 plot_sonda()
 plt.show()
